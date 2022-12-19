5d2c070ba4451c66ed74f7c2967c2ab7d28759c4
student <jadigun319@gmail.com>
2022-12-19 02:00:29 +0000

    Spring Bookzy App Code

---
+++ src/main/java/simplylearn/com/bookzy/BookzyApplication.java
@@ -0,0 +1,13 @@
+package simplylearn.com.bookzy;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BookzyApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BookzyApplication.class, args);
+	}
+
+}
